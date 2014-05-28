384460db6357ee55709e9b4e60432fe461f3077f
Reid Delaney <rdelaney1923@gmail.com>
2014-05-28 15:47:19 -0400

    file for testing graph similarities

---
+++ similarityTest.c
@@ -0,0 +1,181 @@
+/*
+ * file: rotate.c
+ *
+ * Testing the probablistic graph matching algorithm 
+ * by rotating a set of artificial points
+ *
+ * Kevin Liu & Reid Delaney
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+#define PI 3.14159265
+
+/*
+double randomdouble();
+double randomdoubleAngle();
+void pointDistort(int size; double G1[][]; double G2[][]; double centerX; double centerY);
+void rotate(int size; double G1[][]; double G2[][]; double centerX; double centerY); 
+*/
+
+//function returns a random double value in the interval [0, 2PI]
+double randomdoubleAngle(){
+
+	//generate random double between 0 and 1
+	double r = (double)rand()/(double)RAND_MAX;
+
+	//scale to the 0 to 2PI range
+	r *= (2*PI);
+	
+	return r;
+} //end of function
+
+//function returns a random double value in the interval [0,1]
+double randomdouble(){
+
+	//generate random double between 0 and 1
+	double r = (double)rand()/(double)RAND_MAX;
+
+	if(rand()%2 == 0)
+		return r;
+	else
+		return -r;
+} //end of function
+
+//function takes in a set of points, rotates them and then returns the new set
+void rotate(int size, double G1[size][2], double G2[size][2], double centerX, double centerY){
+
+	double theta = randomdoubleAngle();
+	for(int i=0; i < size; i++){
+		G2[i][0] = cos(theta)*(G1[i][0] - centerX) - sin(theta)*(G1[i][1] - centerY) + centerX;
+		G2[i][1] = sin(theta)*(G1[i][0] - centerX) + cos(theta)*(G1[i][1] - centerY) + centerY;
+	}
+} //end of function
+
+void pointDistort(int size, double G1[size][2], double G2[size][2], double centerX, double centerY){
+
+	double distortionX;
+	double distortionY;
+	for(int i=0; i < size; i++){
+		distortionX = randomdouble();
+		distortionY = randomdouble();
+		G2[i][0] = distortionX + G1[i][0];
+		G2[i][1] = distortionY + G1[i][1];
+	}
+
+} //end of function
+
+void graphDistortion(int size, double G1[size][2], double G2[size][2], double centerX, double centerY){
+
+	rotate(size, G1, G2, centerX, centerY);
+	//pointDistort(size, G2, G2, centerX, centerY);
+
+} //end of function
+
+void printMatrix( int size1, int size2, double matrix[size1][size2]) {
+	for (int i=0; i<size1; i++){
+		for(int j=0; j<size2; j++) {
+			//printf("%.2f ", *(matrix + i*size2 + j));
+			printf("%6.2f ", matrix[i][j]);
+		}
+		printf("\n");
+	}
+	printf("\n");
+} //end of function
+
+//create a matrix of distances between nodes
+void neighborDistances(int size, double G1[size][2], double neighbors[size][size]){
+
+	double distance = 0;
+	for(int i = 0; i < size; i++){
+		for(int j = 0; j < size; j++){
+			if(i == j)
+				neighbors[i][j] = 0;
+			else {
+				distance = sqrt((G1[i][0] - G1[j][0])*(G1[i][0] - G1[j][0]) + (G1[i][1] - G1[j][1])*(G1[i][1] - G1[j][1]));
+				neighbors[i][j] = distance;
+			}
+		}
+	}
+
+}
+
+//similarity function
+void similarity(int size, int edges, double neighbors1[size][size], double neighbors2[size][size], double similarity[edges][edges]){
+
+	double simScore = 0;
+	for(int i = 0; i < edges; i++){
+		for(int j = 0; j < edges; j++){
+			for(int k = 0; k < size; k++){
+				for(int l = 0; l < size; l++){
+					if(i == j && k == l)
+						continue;
+					else if(i == k && j == l)
+						continue;
+					else{
+						simScore = exp(-(fabs(neighbors1[i][j] - neighbors2[j][k])));
+						//if(simScore > 0.001)
+							//printf("%8.4f\n", simScore);
+						if(simScore > similarity[i][j])
+							similarity[i][j] = simScore;
+					}
+				}
+			}
+		}
+	}
+
+}
+
+/* make Y an all zero matrix
+ * size1 and size2 are the size of Y
+ */
+void zeros(int size1, int size2, double Y[size1][size2]){
+	for(int i=0; i<size1; i++){
+		for(int j=0; j<size2; j++){
+			//*(Y + (i*size2 + j) ) = 0;
+			Y[i][j] = 0;
+		}
+	}
+}
+
+void main(){
+	double G1[10][2];
+	for(int i=0; i < 10; i++){
+		G1[i][0] = randomdouble();
+		G1[i][1] = randomdouble();
+	}
+	printMatrix(10, 2, G1);
+	double G2[10][2];
+	for(int i=0; i < 10; i++){
+		G2[i][0] = G1[i][0];
+		G2[i][1] = G1[i][1];
+	}
+	graphDistortion(10, G1, G2, 0, 0);
+	printMatrix(10, 2, G2);
+	double neighborDist1[10][10], neighborDist2[10][10];
+	neighborDistances(10, G1, neighborDist1);
+	neighborDistances(10, G2, neighborDist2);
+	
+	printf("neighbor Distances 1\n");
+	printMatrix(10, 10, neighborDist1);
+
+	printf("neighbor distances 2\n");
+	printMatrix(10, 10, neighborDist2);
+
+	double simMatrix[10][10];
+	zeros(10, 10, simMatrix);
+
+	int size2;
+	if(size % 2 == 0)
+		size2 = (size*size)/2 + (size/2);
+	else
+		size2 = (size*size)/2 + (size/2) + 1;
+
+	similarity(10, neighborDist1, neighborDist2, simMatrix);
+	printf("Similarity Scores\n");
+	printMatrix(10, 10, simMatrix);
+
+}
